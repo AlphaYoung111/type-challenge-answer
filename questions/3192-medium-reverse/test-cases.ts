8cbff78d9d28120b332407f32f8d8a247f9de142
AlphaYoung111 <729346015@qq.com>
2022-01-10 22:52:28 +0800

    feat:中等3192-3196

--- questions/3192-medium-reverse/test-cases.ts
+++ questions/3192-medium-reverse/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type Reverse<T extends unknown[], R extends unknown[] = []> = T extends [...infer Rest, infer Last] ? Reverse<Rest, [...R, Last]> : R
+
 type cases = [
   Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
   Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>
--- questions/3196-medium-flip-arguments/test-cases.ts
+++ questions/3196-medium-flip-arguments/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type Reverse<T extends unknown[], R extends unknown[] = []> = T extends [...infer Rest, infer Last] ? Reverse<Rest, [...R, Last]> : R
+
+type FlipArguments<T extends Function> = T extends (...arg: infer P) => infer R ?
+  (...arg: Reverse<P>) => R
+  : T
+
 type cases = [
   Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
   Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
